0804b74cffbf406d5a529df8b60d9238e8700a29
MaquitoGw <maicongoncalves826@gmail.com>
2025-01-19 00:13:58 -0300

    Added error checks to .env controller

--- lib/isconfig.php
+++ lib/isconfig.php
@@ -2,14 +2,32 @@
 namespace lib;
 
 use Dotenv\Dotenv;
+use Exception;
 
 class IsConfig
 {
+    protected static $envLoaded = false;
+
     public static function env($e)
     {
-        // Carrega as variáveis do arquivo .env
-        $dotenv = Dotenv::createImmutable(dirname(__DIR__), '/.env');
-        $dotenv->load();
+        // Verifica se já carregou as variáveis de ambiente anteriormente
+        if (!self::$envLoaded) {
+            $dotenv = Dotenv::createImmutable(dirname(__DIR__), '/.env');
+
+            // Verifica se o arquivo .env existe
+            if (!file_exists(dirname(__DIR__) . '/.env')) {
+                throw new Exception('Arquivo .env não encontrado');
+            }
+
+            // Carrega as variáveis do arquivo .env
+            $dotenv->load();
+            self::$envLoaded = true;
+        }
+
+        // Verifica se a variável existe
+        if (!isset($_ENV[$e])) {
+            throw new Exception("Variável de ambiente {$e} não definida.");
+        }
 
         return $_ENV[$e];
     }
